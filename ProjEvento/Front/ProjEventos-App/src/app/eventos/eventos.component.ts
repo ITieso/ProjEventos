07d798fdff1c49c9f7a9cbc1a1ac133f1b61d781
Igor Henrique de Souza Tieso <igor.tieso@programmers.com.br>
2021-08-10 16:20:10 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- ProjEvento/Front/ProjEventos-App/src/app/eventos/eventos.component.ts
+++ ProjEvento/Front/ProjEventos-App/src/app/eventos/eventos.component.ts
@@ -9,18 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-   eventos: any;
+   eventos: any = [];
+   eventosFiltrados: any = [];  //VARIAVEL QUE ARMAZENA O VALOR QUE EU FILTREI
+   exibirImagem = true;
+   private _filtroLista: string = ''; //VARIAVEL SO PRA ARMAZENA O QUE EU QUERO FILTRAR
+   
+   
+   public get filtroLista() : string{  //ESTE METODO EU RETORNO PARA MEU HTML O EVENTO FILTRADO JA QUE TA ARMAZENADO NO _FILTROlISTA
+    return this._filtroLista; 
+    //PRIMEIRO EU ATRIBUO O VALOR PARA O SET, E DEPOIS EU USO ESSE GET PRA PEGAR O VALOR DO SET E MANDAR PARA O HTML
+   }
 
-  constructor(private http: HttpClient) { }
+   public set filtroLista(value: string){//ESTE METODO EU PEGO O VALOR QUE VEM DO HTML E ATRIBUO PRA VARIAVEL _FILTROLISTA
+     this._filtroLista = value;
+     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+     //LINHA 23 EU ATRIBUO PARA MEU EVENTOSFILTRADOS  O RETORNO DO EVENTO QUE VEM DA FUNCAO FILTRAREVENTOS, EU MANDO PRA ELA FILTRAR E PEGO O QUE VEM
+   }
 
-  ngOnInit(): void {
-    this.getEventos();
+   
+   constructor(private http: HttpClient) { }
+   ngOnInit(): void {
+     this.getEventos();
+    }
+
+    filtrarEventos(filtrar : string) : any {
+      filtrar = filtrar.toLocaleLowerCase();
+        return this.eventos.filter(	
+            (evento : any)  => evento.tema.toLocaleLowerCase().indexOf(filtrar) !== -1 
+            || evento.local.toLocaleLowerCase().indexOf(filtrar) !== -1)
+    }
+ 
+    mostrarImagem(){
+      this.exibirImagem = !this.exibirImagem 
   }
 
- getEventos(): void{
-    this.http.get('http://localhost:5000/api/Eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-    )
-    }
+    getEventos(): void{
+     this.http.get('http://localhost:5000/api/Eventos').subscribe(
+        response => {
+          this.eventos = response;
+          this.eventosFiltrados = this.eventos;
+          },
+          error => console.log(error)
+          )}
 }
